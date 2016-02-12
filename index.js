b866027e11c7e54277e28edd3a2d9734fab65e18
Lane Harrison <lanetharrison@gmail.com>
2016-02-12 09:49:08 -0500

    Re-arranges node imports

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var app = express();
-var server = http.Server(app);
+var http       = require('http');
+var path       = require('path');
+var express    = require('express');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 12345;
 
+var app = express();
+var server = http.Server(app);
+
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-//app.use(express.bodyParser());
-
 app.post('/myaction', function(req, res) {
+  // TODO SAVE TO DATABASE (SQLITE?)
+
+  // TODO SEND RESPONSE OF confirmation to user
   res.send('You sent the name "' + req.body.name + '".');
+  res.send('You sent the email "' + req.body.email + '".');
+  res.send('You sent the message "' + req.body.message + '".');
 });
 
 app.use(express.static(path.join(__dirname, 'public/')));
