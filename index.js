84b1228b9f28a2fa6224b0284f11756f2af728a5
mgaskell <mgaskell@wpi.edu>
2016-02-12 06:12:13 -0800

    Add .gitignore and node files

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var server = http.Server(app);
+var bodyParser = require('body-parser');
+var app     = express();
 
 var port = process.env.PORT || 12345;
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//app.use(express.bodyParser());
+
+app.post('/myaction', function(req, res) {
+  res.send('You sent the name "' + req.body.name + '".');
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
