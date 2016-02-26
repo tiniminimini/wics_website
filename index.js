c29175dccf531e195a30cc2cd29ad5fa2ee714bd
mgaskell <mgaskell@wpi.edu>
2016-02-26 10:04:48 -0800

    final modifications and event list

--- index.js
+++ index.js
@@ -2,26 +2,40 @@ var http       = require('http');
 var path       = require('path');
 var express    = require('express');
 var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
 
 var port = process.env.PORT || 12345;
 
 var app = express();
 var server = http.Server(app);
 
+var db = new sqlite3.Database('wics_db.sqlite');
+db.run("CREATE TABLE IF NOT EXISTS contactRequest (name TEXT, email TEXT, message TEXT)");
+
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.post('/myaction', function(req, res) {
   // TODO SAVE TO DATABASE (SQLITE?)
+  console.log('You sent the name "' + req.body.name + '".');
+  console.log('You sent the email "' + req.body.email + '".');
+  console.log('You sent the message "' + req.body.message + '".');
 
+	db.run ("INSERT INTO contactRequest (name, email, message) VALUES (?, ?, ?)", req.body.name, req.body.email, req.body.message);
+
+	db.each("SELECT name, email, message FROM contactRequest", function(err, row) {
+      console.log(row.name);
+      console.log(row.email);
+      console.log(row.message);
+  	});
   // TODO SEND RESPONSE OF confirmation to user
-  res.send('You sent the name "' + req.body.name + '".');
-  res.send('You sent the email "' + req.body.email + '".');
-  res.send('You sent the message "' + req.body.message + '".');
+  res.write('You sent the name "' + req.body.name + '".');
+  res.write('You sent the email "' + req.body.email + '".');
+  res.end('You sent the message "' + req.body.message + '".');
 });
 
 app.use(express.static(path.join(__dirname, 'public/')));
 
 server.listen(port, function(){
 	console.log('App is listening on port ' + port);
-
 });
+
